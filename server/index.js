c46a9320a0011efdb032054fc3b7769276fcf66f
Vedant Lohbare <vedant.lohbare@gmail.com>
2022-03-13 14:14:37 +0530

    search api done

--- server/index.js
+++ server/index.js
@@ -5,7 +5,16 @@ const userData = require("./data/userData.json");
 
 app.get("/", (req, res) => {
   const query = req.query.query;
-  res.send(userData);
+  // Get the query params from the get call
+  if (query) {
+    // Run search only if the query is present
+    var filteredUsers = userData.filter((user) =>
+      user.name.toLowerCase().includes(query.toLowerCase())
+    );
+    // Creating a new variable filteredUsers if it includes the characters present in the query stirng
+    res.send(filteredUsers);
+    // Send the filteredUsers in JSON format
+  }
 });
 
 app.listen(PORT, () => {
